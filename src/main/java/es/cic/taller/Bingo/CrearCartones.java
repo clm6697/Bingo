df9971674248399aa202985fbe4096edab1c9cb0
clm6697 <6697clm@gmail.com>
2017-07-18 12:47:25 +0200

    SEgunda

---
+++ src/main/java/es/cic/taller/Bingo/CrearCartones.java
@@ -0,0 +1,29 @@
+package es.cic.taller.Bingo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CrearCartones {
+	Random rnd = new Random();
+	
+	for (int i=0; i<=14; i++) {
+		List<Numero> Carton = new ArrayList<>();
+		
+		Numero aleatorio = new Numero();
+		//int aleatorio = rnd.nextInt()*90+1;
+		if (i==0) {
+			Carton.add(i, aleatorio);
+		}
+		else {
+			
+		}
+		 
+		 
+		 
+		 
+	}
+	
+	
+
+}
---
+++ src/main/java/es/cic/taller/Bingo/Numero.java
@@ -0,0 +1,38 @@
+package es.cic.taller.Bingo;
+
+
+public class Numero {
+	
+	private int mNumero;
+	
+	public void setmNumero(int mNumero) {
+		this.mNumero = mNumero;
+	}
+	public Numero(int Numero) {
+		mNumero= Numero;
+	}
+	public int getNumero() {
+		return mNumero;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mNumero;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Numero other = (Numero) obj;
+		if (mNumero != other.mNumero)
+			return false;
+		return true;
+	}
+
+}
